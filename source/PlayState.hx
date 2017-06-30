2f9c51885cbdc1d583ce40ff87c3a3a3947bbf0f
Murilo Cadanus da Costa <murilocadanus@gmail.com>
2017-06-30 16:25:59 -0400

    [ADD] Created labels for players score.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,12 +2,29 @@ package;
 
 import flixel.FlxState;
 import flixel.FlxG;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
+	private static inline var SCORE_TEXT_SIZE:Int = 130;
+	private var _player1Score:Int = 0;
+	private var _player1ScoreText:FlxText;
+
+	private var _player2Score:Int = 0;
+	private var _player2ScoreText:FlxText;
+
 	override public function create():Void
 	{
+		// Disable mouse cursor
 		FlxG.mouse.visible = false;
+
+		// Add score label for player 1
+		_player1ScoreText = new FlxText(0, 0, SCORE_TEXT_SIZE, "Player 1 score: " + _player1Score);
+		add(_player1ScoreText);
+
+		// Add score label for player 2
+		_player2ScoreText = new FlxText(FlxG.width - SCORE_TEXT_SIZE, 0, SCORE_TEXT_SIZE, "Player 2 score: " + _player2Score);
+		add(_player2ScoreText);
 	}
 
 	override public function update(elapsed:Float):Void
