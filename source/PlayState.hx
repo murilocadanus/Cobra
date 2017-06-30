36c0bfd8a742b591ccca3de587629e18687f7509
Murilo Cadanus da Costa <murilocadanus@gmail.com>
2017-06-30 17:37:32 -0400

    [ADD] Created players controls.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,19 +5,27 @@ import flixel.FlxG;
 import flixel.text.FlxText;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
+import flixel.FlxObject;
+import flixel.util.FlxSpriteUtil;
+import flixel.util.FlxTimer;
 
 class PlayState extends FlxState
 {
 	private static inline var SCORE_TEXT_SIZE:Int = 130;
 	private static inline var BLOCK_SIZE:Int = 8;
+	private var _movementInterval:Float = 8;
 
 	private var _player1Score:Int = 0;
 	private var _player1ScoreText:FlxText;
 	private var _player1Head:FlxSprite;
+	private var _player1CurrentDirection = FlxObject.UP;
+	private var _player1NextDirection:Int = FlxObject.UP;
 
 	private var _player2Score:Int = 0;
 	private var _player2ScoreText:FlxText;
 	private var _player2Head:FlxSprite;
+	private var _player2CurrentDirection = FlxObject.DOWN;
+	private var _player2NextDirection:Int = FlxObject.DOWN;
 
 	override public function create():Void
 	{
@@ -51,16 +59,106 @@ class PlayState extends FlxState
 
 		// Add player 2 snake head to the board
 		add(_player2Head);
+
+		// Start to move player 1
+		movePlayer1();
+
+		// Start to move player 2
+		movePlayer2();
+
+		// Set a timer to move
+		timerTick();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+
+		// WASD keys controls player 1
+		if (FlxG.keys.pressed.W && _player1CurrentDirection != FlxObject.DOWN)
+		{
+			_player1NextDirection = FlxObject.UP;
+		}
+		else if (FlxG.keys.pressed.S && _player1CurrentDirection != FlxObject.UP)
+		{
+			_player1NextDirection = FlxObject.DOWN;
+		}
+		else if (FlxG.keys.pressed.A && _player1CurrentDirection != FlxObject.RIGHT)
+		{
+			_player1NextDirection = FlxObject.LEFT;
+		}
+		else if (FlxG.keys.pressed.D && _player1CurrentDirection != FlxObject.LEFT)
+		{
+			_player1NextDirection = FlxObject.RIGHT;
+		}
+
+		// Arrow keys to controls player 2
+		if (FlxG.keys.pressed.UP && _player2CurrentDirection != FlxObject.DOWN)
+		{
+			_player2NextDirection = FlxObject.UP;
+		}
+		else if (FlxG.keys.pressed.DOWN && _player2CurrentDirection != FlxObject.UP)
+		{
+			_player2NextDirection = FlxObject.DOWN;
+		}
+		else if (FlxG.keys.pressed.LEFT && _player2CurrentDirection != FlxObject.RIGHT)
+		{
+			_player2NextDirection = FlxObject.LEFT;
+		}
+		else if (FlxG.keys.pressed.RIGHT && _player2CurrentDirection != FlxObject.LEFT)
+		{
+			_player2NextDirection = FlxObject.RIGHT;
+		}
 	}
 
 	private function centerSprite(Sprite:FlxSprite):Void
 	{
 		Sprite.offset.set(1, 1);
 		Sprite.centerOffsets();
+	}
+
+	private function timerTick(?Timer:FlxTimer):Void
+	{	
+		new FlxTimer().start(_movementInterval / FlxG.updateFramerate, timerTick);
+		movePlayer1();
+		movePlayer2();
+	}
+
+	private function movePlayer1():Void
+	{
+		// Update player 1 position
+		switch (_player1NextDirection)
+		{
+			case FlxObject.LEFT:
+				_player1Head.x -= BLOCK_SIZE;
+			case FlxObject.RIGHT:
+				_player1Head.x += BLOCK_SIZE;
+			case FlxObject.UP:
+				_player1Head.y -= BLOCK_SIZE;
+			case FlxObject.DOWN:
+				_player1Head.y += BLOCK_SIZE;
+		}
+		_player1CurrentDirection = _player1NextDirection;
+
+		FlxSpriteUtil.screenWrap(_player1Head);
+	}
+
+	private function movePlayer2():Void
+	{
+		// Update player 2 position
+		switch (_player2NextDirection)
+		{
+			case FlxObject.LEFT:
+				_player2Head.x -= BLOCK_SIZE;
+			case FlxObject.RIGHT:
+				_player2Head.x += BLOCK_SIZE;
+			case FlxObject.UP:
+				_player2Head.y -= BLOCK_SIZE;
+			case FlxObject.DOWN:
+				_player2Head.y += BLOCK_SIZE;
+		}
+		_player2CurrentDirection = _player2NextDirection;
+
+		FlxSpriteUtil.screenWrap(_player2Head);
 	}
 }
