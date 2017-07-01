0b695176040ec0aba4ac93346ee05738c60b9694
Murilo Cadanus da Costa <murilocadanus@gmail.com>
2017-06-30 21:22:12 -0400

    [ADD] Created collision with fruits and snakes heads, increasing points also.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,24 +8,32 @@ import flixel.util.FlxColor;
 import flixel.FlxObject;
 import flixel.util.FlxSpriteUtil;
 import flixel.util.FlxTimer;
+import flixel.system.FlxAssets;
+import flixel.group.FlxSpriteGroup;
 
 class PlayState extends FlxState
 {
+	private static inline var MIN_INTERVAL:Float = 2;
 	private static inline var SCORE_TEXT_SIZE:Int = 130;
 	private static inline var BLOCK_SIZE:Int = 8;
+	
 	private var _movementInterval:Float = 8;
 
 	private var _player1Score:Int = 0;
 	private var _player1ScoreText:FlxText;
 	private var _player1Head:FlxSprite;
+	private var _player1Body:FlxSpriteGroup;
 	private var _player1CurrentDirection = FlxObject.UP;
 	private var _player1NextDirection:Int = FlxObject.UP;
 
 	private var _player2Score:Int = 0;
 	private var _player2ScoreText:FlxText;
 	private var _player2Head:FlxSprite;
+	private var _player2Body:FlxSpriteGroup;
 	private var _player2CurrentDirection = FlxObject.DOWN;
 	private var _player2NextDirection:Int = FlxObject.DOWN;
+
+	private var _fruit:FlxSprite;
 
 	override public function create():Void
 	{
@@ -60,6 +68,13 @@ class PlayState extends FlxState
 		// Add player 2 snake head to the board
 		add(_player2Head);
 
+		// Create a collectable fruit
+		_fruit = new FlxSprite();
+		_fruit.makeGraphic(BLOCK_SIZE - 2, BLOCK_SIZE - 2, FlxColor.RED);
+		spawnFruit();
+		centerSprite(_fruit);
+		add(_fruit);
+
 		// Start to move player 1
 		movePlayer1();
 
@@ -73,6 +88,12 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+
+		// Check if player 1 collected a fruit
+		FlxG.overlap(_player1Head, _fruit, player1EatFruit);
+
+		// Check if player 2 collected a fruit
+		FlxG.overlap(_player2Head, _fruit, player2EatFruit);
 
 		// WASD keys controls player 1
 		if (FlxG.keys.pressed.W && _player1CurrentDirection != FlxObject.DOWN)
@@ -160,5 +181,65 @@ class PlayState extends FlxState
 		_player2CurrentDirection = _player2NextDirection;
 
 		FlxSpriteUtil.screenWrap(_player2Head);
+	}
+
+	private function player1EatFruit(Snake:FlxObject, Fruit:FlxObject):Void
+	{
+		// Update the score
+		_player1Score += 10;
+		_player1ScoreText.text = "Player 1 score: " + _player1Score;
+		
+		// Play a sound
+		FlxG.sound.load(FlxAssets.getSound("flixel/sounds/beep")).play();
+		
+		// TODO: grow the snake
+		
+		// Become faster each pickup - set a max speed though!
+		if (_movementInterval >= MIN_INTERVAL)
+		{
+			_movementInterval -= 0.25;
+		}
+
+		// Spawn a new fruit
+		spawnFruit();
+	}
+
+	private function player2EatFruit(Snake:FlxObject, Fruit:FlxObject):Void
+	{
+		// Update the score
+		_player2Score += 10;
+		_player2ScoreText.text = "Player 2 score: " + _player2Score;
+		
+		// Play a sound
+		FlxG.sound.load(FlxAssets.getSound("flixel/sounds/beep")).play();
+
+		// TODO: grow the snake
+		
+		// Become faster each pickup - set a max speed though!
+		if (_movementInterval >= MIN_INTERVAL)
+		{
+			_movementInterval -= 0.25;
+		}
+
+		// Spawn a new fruit
+		spawnFruit();
+	}
+
+	private function increasePlayer1Unit():Void
+	{
+		// Increase snake unit
+		var snakeUnit:FlxSprite = new FlxSprite(-20, -20);
+		segment.makeGraphic(BLOCK_SIZE - 2, BLOCK_SIZE - 2, FlxColor.GREEN); 
+		_snakeBody.add(segment);
+	}
+
+	private function spawnFruit(?fruitPos:FlxObject, ?playerPos:FlxObject):Void
+	{
+		// Pick a random place to put the fruit down
+		_fruit.x = FlxG.random.int(0, Math.floor(FlxG.width / 8) - 1) * 8;
+		_fruit.y = FlxG.random.int(0, Math.floor(FlxG.height / 8) - 1) * 8;
+		
+		// Check that the coordinates we picked aren't already covering the snake, if they are then run this function again
+		FlxG.overlap(_fruit, _player1Head, spawnFruit);
 	}
 }
